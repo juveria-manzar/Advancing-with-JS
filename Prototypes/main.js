575d9075543d1a1821a814422217e956f3aaa6ff
Juveria Manzar <juveriadot@gmail.com>
2020-04-08 20:49:49 +0530

    prototypes

--- Prototypes/main.js
+++ Prototypes/main.js
@@ -141,4 +141,71 @@ __proto__ property: We will discuss this while explaining inheritance in JavaScr
 
 
 
-/**Prototype object of the constructor function is shared among all the objects created using the constructor function. */
+/**Prototype object of the constructor function is shared among all the objects created using the constructor function. *
+ * Thus to avoid repitition and redundancy we use protype objects to create objects of a particular function (like class)*/
+
+
+/**As prototype object is an object, we can attach properties and methods to the prototype object. 
+ * Thus, enabling all the objects created using the constructor function to share those properties and methods.
+ * The new property can be added to the constructor function’s prototype property using either the dot notation or 
+ * square bracket notation*/
+function Human(firstName, lastName) {
+    this.firstName = firstName,
+        this.lastName = lastName,
+        this.fullName = function() {
+            return this.firstName + " " + this.lastName;
+        }
+}
+
+//Dot notation
+Human.prototype.name = "Ashwin";
+console.log(Human.prototype.name) //Output: Ashwin
+
+//Square bracket notation
+Human.prototype["age"] = 26;
+console.log(Human.prototype["age"]); //Output: 26
+
+console.log(Human.prototype);
+//Create an empty constructor function
+function Person() {
+
+}
+//Add property name, age to the prototype property of the Person constructor function
+Person.prototype.name = "Ashwin";
+Person.prototype.age = 26;
+Person.prototype.sayName = function() {
+    console.log(this.name);
+}
+
+//Create an object using the Person constructor function
+var person1 = new Person();
+
+//Access the name property using the person object
+console.log(person1.name) // Output" Ashwin
+
+console.log(person1) //undefined
+
+//So how does the output of console.log(person.name) was “Ashwin”?
+
+/**When we try to access a property of an object, JavaScript engines first tries to find the property on the object,
+ *  if the property is present on the object it outputs its value. But, if the property is not present on the object 
+ * then it tries to find the property on the prototype object or dunder proto of the object. If the property is found 
+ * the value is returned else JavaScript engine tries to find the property on the dunder proto of the object. 
+ * This chain continues until the dunder proto property is null. In these cases, the output will be undefined. */
+
+
+//create another object person2 using the Person constructor function.
+var person2 = new Person();
+//Access the name property using the person2 object
+console.log(person2.name) // Output: Ashwin
+
+//Now, define a property name on the person1 object
+person1.name = "Anil"
+console.log(person1.name) //Output: Anil
+console.log(person2.name) //Output: Ashwin
+
+/**Here person1.name outputs “Anil”. As mentioned earlier, JavaScript engine first tries 
+ * to find the property on the object itself. In this case, name property is present on the object person1 itself, 
+ * hence JavaScript engines outputs value of name property of person1.
+In the case of person2, the name property is not present on the object. Hence, it outputs person2’s prototype 
+object’s property name. */
