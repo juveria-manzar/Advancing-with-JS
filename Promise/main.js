f2f6f784376037d349b6b636bc0722d66412bb7f
Juveria Manzar <juveriamanzar29@gmail.com>
2021-06-17 04:29:12 +0530

    understanding promises

--- Promise/main.js
+++ Promise/main.js
@@ -1,7 +1,7 @@
 let fs=require('fs');
 
 function task(path){
-    return new Promise(function executor(resolve,reject){
+    return new Promise(function(resolve,reject){
         fs.readFile(path,function(err,data){
             if (err) reject(err);
             else resolve(data);
