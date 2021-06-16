fbd3e9b19807bfbdb9c52fb6491450ec11f11694
Juveria Manzar <juveriamanzar29@gmail.com>
2021-06-17 04:26:46 +0530

    understanding promises

---
+++ Promise/main.js
@@ -0,0 +1,21 @@
+let fs=require('fs');
+
+function task(path){
+    return new Promise(function executor(resolve,reject){
+        fs.readFile(path,function(err,data){
+            if (err) reject(err);
+            else resolve(data);
+        })
+    }).then(function(data){
+        console.log(data+"");
+    }).catch(function(err){
+        fs.writeFile(path, 'Undertanding promises', function (err) {
+            if (err) throw err;
+            console.log('File is created successfully.');
+        });
+    })
+}
+
+task('f1.txt');
+task('f4.txt');
+task('f3.txt');
